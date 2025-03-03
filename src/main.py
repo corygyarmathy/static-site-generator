3f4c3258189607dfa4ff3ea4b8ab65665cb70fd6
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-03 19:29:52 +0800

    feat: create main.py w/ basic scaffolding

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    textnode = TextNode(
+        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
+    )
+    print(textnode)
+
+
+if __name__ == "__main__":
+    _ = main()
