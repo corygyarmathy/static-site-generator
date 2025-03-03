6decdb53bd39491fe9a79933a0998a6bc2bc55bc
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-03 19:29:39 +0800

    feat: create TextType class

---
+++ src/textnode.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from typing import Any, override
+
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(
+        self, text: str, text_type: TextType = TextType.NORMAL, url: str | None = None
+    ):
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
+
+    @override
+    def __eq__(self, other) -> bool:
+        if type(other) is TextNode:
+            return (
+                self.text == other.text
+                and self.text_type == other.text_type
+                and self.url == other.url
+            )
+
+        return False
+
+    @override
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
