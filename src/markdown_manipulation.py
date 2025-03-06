6e19bcafaf3af56097a8a92b26e47e0829b3fb52
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-06 15:43:23 +0800

    feat: created split_nodes_image function
    - Used for splitting text with markdown-style image links into the a
    list of TextNodes of the appropriate TextType
    - Included relevant tests

--- src/markdown_manipulation.py
+++ src/markdown_manipulation.py
@@ -25,6 +25,32 @@ def split_nodes_delimiter(
     return new_nodes
 
 
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for old_node in old_nodes:
+        text = old_node.text
+        img_matches = extract_markdown_images(text)
+
+        for img_match in img_matches:
+            split_nodes = []
+            img_text = f"![{img_match[0]}]({img_match[1]})"
+            sections = text.split(f"![{img_match[0]}]({img_match[1]})", 1)
+            # if len(img_matches[i]) > j + 1:
+            #     text = sections[j]
+            # if len(sections) % 2 == 0:
+            #     raise ValueError("invalid markdown, formatted section not closed")
+            for i in range(len(sections)):
+                # if sections[i] == "":
+                #     continue
+                if i % 2 == 0:
+                    split_nodes.append(TextNode(sections[i], TextType.TEXT))
+                    text = sections[i + 1]
+                else:
+                    split_nodes.append(
+                        TextNode(img_match[0], TextType.IMAGE, img_match[1])
+                    )
+            new_nodes.extend(split_nodes)
+    return new_nodes
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,6 +4,7 @@ from markdown_manipulation import (
     extract_markdown_images,
     extract_markdown_links,
     split_nodes_delimiter,
+    split_nodes_image,
 )
 from textnode import TextNode, TextType, text_node_to_html_node
 
@@ -192,6 +193,51 @@ class TestTextNode(unittest.TestCase):
                 ("to youtube", "https://www.youtube.com/@bootdotdev"),
             ],
             matches,
+        )
+
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_image_single(self):
+        node = TextNode(
+            "![image](https://www.example.COM/IMAGE.PNG)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
         )
 
 
