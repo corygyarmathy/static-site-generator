4032f2752f660e590ddb870ff86cbe2ca6167d69
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-07 11:19:23 +0800

    test: scaffolding for objects in main as part of testing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from markdown_manipulation import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes,
+)
 from textnode import TextNode, TextType
 
 
@@ -10,14 +16,41 @@ def main():
     htmlnode = HTMLNode(
         "a", "text", None, {"href": "https://www.google.com", "target": "_blank"}
     )
-    # print(htmlnode)
+    print(htmlnode)
 
     leafnode = LeafNode("a", "text", {"href": "https://www.google.com"})
-    # print(leafnode.to_html())
+    print(leafnode.to_html())
 
     parentnode = ParentNode("b", [leafnode, leafnode])
     grandparentnode = ParentNode("div", [parentnode])
     print(grandparentnode.to_html())
+
+    node = TextNode("**bold** and _italic_", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+
+    img_node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_img_node = split_nodes_image([img_node])
+    print(new_img_node.__repr__())
+
+    link_node = TextNode(
+        "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_link_node = split_nodes_link([link_node])
+    print(new_link_node.__repr__())
+
+    single_image_node = TextNode(
+        "![image](https://www.example.COM/IMAGE.PNG)",
+        TextType.TEXT,
+    )
+    new_single_image_nodes = split_nodes_image([single_image_node])
+
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    new_text_nodes = text_to_textnodes(text)
 
 
 if __name__ == "__main__":
