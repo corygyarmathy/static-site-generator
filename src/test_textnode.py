84a18b9c43647f27fd612986e85ebf1c78832ea4
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-07 11:15:01 +0800

    feat: add text_to_textnodes function
    - Converts text (str) in Markdown format to TextNode objects, with the
    appropriate TextType

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,6 +6,7 @@ from markdown_manipulation import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 from textnode import TextNode, TextType, text_node_to_html_node
 
@@ -257,6 +258,27 @@ class TestTextNode(unittest.TestCase):
                 ),
             ],
         )
+
+    def test_text_to_text_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
         )
 
 
