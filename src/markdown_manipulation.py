6ea174c4c27e8c5cb25a0b4160f9d7a59a5e642a
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-07 10:59:30 +0800

    fix: corrected split_nodes_image logic to address edge cases

--- src/markdown_manipulation.py
+++ src/markdown_manipulation.py
@@ -28,28 +28,30 @@ def split_nodes_delimiter(
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes: list[TextNode] = []
     for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
         text = old_node.text
         img_matches = extract_markdown_images(text)
+        if len(img_matches) == 0:
+            new_nodes.append(old_node)
+            continue
 
         for img_match in img_matches:
+            img_text: str = img_match[0]
+            img_url: str = img_match[1]
+            sections = text.split(f"![{img_text}]({img_url})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":  # Check for: image, no text
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(img_text, TextType.IMAGE, img_url))
+            text = sections[1]
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
             split_nodes = []
-            img_text = f"![{img_match[0]}]({img_match[1]})"
-            sections = text.split(f"![{img_match[0]}]({img_match[1]})", 1)
-            # if len(img_matches[i]) > j + 1:
-            #     text = sections[j]
-            # if len(sections) % 2 == 0:
-            #     raise ValueError("invalid markdown, formatted section not closed")
-            for i in range(len(sections)):
-                # if sections[i] == "":
-                #     continue
-                if i % 2 == 0:
-                    split_nodes.append(TextNode(sections[i], TextType.TEXT))
-                    text = sections[i + 1]
-                else:
-                    split_nodes.append(
-                        TextNode(img_match[0], TextType.IMAGE, img_match[1])
-                    )
-            new_nodes.extend(split_nodes)
     return new_nodes
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
