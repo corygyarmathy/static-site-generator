cbf0806c22610d11af39a40203d151f3c6e79197
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-11 17:44:05 +0800

    fix: incorrect behaviour when converting TextNode of TextType CODE to HTMLNode
    - Was using the type of text as the value, rather than the text itself

--- src/textnode.py
+++ src/textnode.py
@@ -89,7 +89,7 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
         case TextType.ITALIC:
             return LeafNode(tag="i", value=text_node.text)
         case TextType.CODE:
-            return LeafNode(tag="code", value=text_node.text_type.value)
+            return LeafNode(tag="code", value=text_node.text)
         case TextType.LINK:
             return LeafNode(
                 tag="a",
