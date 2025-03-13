e2f57696533fef5828b681e70ea1605a787c9c8c
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-13 09:16:16 +0800

    feat: overwrite files in public/ with those in static/

---
+++ src/file_manipulation.py
@@ -0,0 +1,32 @@
+from os import chdir, getcwd, listdir, mkdir, path
+from shutil import copy, rmtree
+import subprocess
+
+
+def get_git_root():
+    return subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True
+    ).stdout.strip()
+
+
+def overwrite_directory_files(source: str, destination: str) -> None:
+    git_root = get_git_root()
+    chdir(git_root)
+    print(f"Current working directory: {getcwd()}")
+    if path.exists(destination):
+        rmtree(destination)
+        mkdir(destination)
+    copy_files(source, destination)
+
+
+def copy_files(source: str, destination: str) -> None:
+    dir_items = listdir(source)
+    for item in dir_items:
+        source_path = path.join(source, item)
+        dest_path = path.join(destination, item)
+        if path.isfile(source_path):  # File
+            log: str = copy(source_path, dest_path)
+            print(log)
+        else:  # Directory
+            mkdir(dest_path)
+            copy_files(source_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,58 +1,10 @@
-from htmlnode import HTMLNode, LeafNode, ParentNode
-from markdown_manipulation import (
-    split_nodes_delimiter,
-    split_nodes_image,
-    split_nodes_link,
-    markdown_to_textnodes,
-)
-from markdown_blocks import markdown_to_blocks
-from textnode import TextNode, TextType
+from os import getcwd
+
+from file_manipulation import overwrite_directory_files
 
 
 def main():
-    """Temporary scaffolding function for debugging."""
-    textnode = TextNode(
-        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
-    )
-    print(textnode)
-    htmlnode = HTMLNode(
-        "a", "text", None, {"href": "https://www.google.com", "target": "_blank"}
-    )
-    print(htmlnode)
-
-    leafnode = LeafNode("a", "text", {"href": "https://www.google.com"})
-    print(leafnode.to_html())
-
-    parentnode = ParentNode("b", [leafnode, leafnode])
-    grandparentnode = ParentNode("div", [parentnode])
-    print(grandparentnode.to_html())
-
-    node = TextNode("**bold** and _italic_", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
-
-    img_node = TextNode(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-        TextType.TEXT,
-    )
-    new_img_node = split_nodes_image([img_node])
-    print(new_img_node.__repr__())
-
-    link_node = TextNode(
-        "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
-        TextType.TEXT,
-    )
-    new_link_node = split_nodes_link([link_node])
-    print(new_link_node.__repr__())
-
-    single_image_node = TextNode(
-        "![image](https://www.example.COM/IMAGE.PNG)",
-        TextType.TEXT,
-    )
-    new_single_image_nodes = split_nodes_image([single_image_node])
-
-    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    new_text_nodes = markdown_to_textnodes(text)
+    overwrite_directory_files("static", "public")
 
 
 if __name__ == "__main__":
